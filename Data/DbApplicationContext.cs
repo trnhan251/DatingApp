bdbf09d92f2753aae1096fdc5e91ba3effa0780b
Nhan Vu <nhan997@gmail.com>
2020-02-26 10:07:59 +0100

    First commit

---
+++ Data/DbApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class DbApplicationContext : DbContext
+    {
+        public DbApplicationContext(DbContextOptions<DbApplicationContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
